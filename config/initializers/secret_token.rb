11879ca9f375f96a2b846708eaae2f6faadf96e8
Nathalie Wong <nathalie.wong@wildfireapp.com>
2012-06-16 16:15:08 +0100

    first commit for nugget

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nugget::Application.config.secret_token = 'bf06fe0dd3d559d25e55a1f45d06406b4630eff2071c552c18a50ee18484b1f960e54cf473b214bafb9e3b1c964f2dc03ac9e1ee2c09494ce173f483442e9d78'
