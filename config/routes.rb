4d07ed83a03db78043f7baa776330332e1014241
Nathalie Wong <nathalie.wong@wildfireapp.com>
2012-07-26 16:35:50 +0100

    first attempts at unit and functional testing

--- config/routes.rb
+++ config/routes.rb
@@ -55,7 +55,7 @@ Nugget::Application.routes.draw do
   # root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
-
+  root :to => 'users#index'
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,6 +1,19 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+  test "name cannot be blank" do
+    assert true
+  end
+
+  test "should not save blank name" do
+    user = User.new
+    assert !user.save, "saved while name field was blank"
+  end
+
+  test "should report error" do
+    undefined_variable
+    assert true
+  end
   # test "the truth" do
   #   assert true
   # end
