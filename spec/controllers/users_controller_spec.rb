0042816229893163fde2ecebaf7bceb866dfdec4
Nathalie Wong <nathalie.wong@wildfireapp.com>
2012-07-29 20:36:40 +0100

    created user factory, users controller test not passing

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,16 +2,28 @@ require 'spec/spec_helper'
 
 describe UsersController do
   describe "GET index" do
-    it "assigns @users" do
-      user = User.create(:name => "foo bar")
+    before (:each) do
+      @user = FactoryGirl.create(:user)
       get :index
-      assigns(:users).should eq([user])
-    end
-
-    it "renders the index template" do
-      get :index
-      response.should render_template("index")
+      assign(:users, @user)
+      render :action => 'index'
+      #assigns(:users).should eq([user])
     end
   end
+
+  it "renders the index template" do
+    get :index
+    response.should render_template("index")
+  end
+
+  #describe "GET show" do
+   # before (:each) do
+    #  @user = Factory(:user)
+     # get :show, 
 end
+
+# commit this
+# use factory girl in this spec
+# write specs for the other actions in this controller
+# write a spec for the other controller
   
