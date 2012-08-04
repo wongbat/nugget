8b3fa1d5bf053c2d188df5429319c3b1f4a8a6b9
Nathalie Wong <nathalie.wong@wildfireapp.com>
2012-08-04 17:58:46 +0100

    tests added for user associations

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -1,6 +1,18 @@
-require 'spec/spec_helper'
+require 'spec_helper'
 
 describe Micropost do
+
+  describe "has a valid user" do
+    FactoryGirl.create(:micropost)
+    it { should validate_presence_of(:user) }
+    it { should belong_to(:user) }
+  end
+  
+  describe "it does not have a valid user" do
+    FactoryGirl.create(:invalid_micropost)
+    it {should_not be_valid}
+  end
+
   describe "content" do
     it "should not save where content > 140 characters" do
       content = "z"*141
@@ -11,7 +23,7 @@ describe Micropost do
     it "should save where content < 140 characters" do
       content = "ABC"
       micropost = Micropost.new(:content => content)
-      assert micropost.save
+      micropost.save
     end
   end
 end
