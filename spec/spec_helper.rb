25c92535215b47c16a37c03dcc0cb94de3647bc0
Nathalie Wong <nathalie.wong@wildfireapp.com>
2012-08-04 17:52:43 +0100

    using shoulda gem

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,7 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
 require 'factory_girl'
+require 'shoulda'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
