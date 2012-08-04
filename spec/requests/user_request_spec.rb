2a63e24f2cfdcfa0bff86446715da8d54d84c5de
Nathalie Wong <nathalie.wong@wildfireapp.com>
2012-08-04 10:51:21 +0100

    all request specs for Users

--- spec/requests/user_request_spec.rb
+++ spec/requests/user_request_spec.rb
@@ -3,6 +3,8 @@ require 'spec_helper'
 describe "Users" do
   it "can add new user" do
     visit '/users'
+    page.should have_css('h1', :text => 'Listing users')
+    page.should have_css("table", :text => "Name", :text => "Email")
     click_link "New User"
     fill_in "Name", :with => "Nat"
     fill_in "Email", :with => "Foo"
@@ -15,12 +17,26 @@ describe "Users" do
   it "allows users to change their details" do
     FactoryGirl.create(:user)
     visit '/users'
-    debugger
     click_link "Edit"
     fill_in "Name", :with => "new name"
     fill_in "Email", :with => "new@email.com"
     click_button "Update User"
     page.body.should include("User was successfully updated.")
   end
-end
 
+  it "displays user's details on it's own" do
+    FactoryGirl.create(:user)
+    visit '/users'
+    click_link "Show"
+    page.should have_content("Name: Mog")
+    page.should have_content("Email: mog@hotmail.com")
+  end
+
+  it "deletes the user" do
+    FactoryGirl.create(:user)
+    visit '/users'
+    click_link "Destroy"
+    page.body.should_not have_content("Mog")
+    page.body.should_not have_content("mog@hotmail.com")
+  end
+end
