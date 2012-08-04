7e176d9407d37c91b10b0c111b0421ed08f7b688
Nathalie Wong <nathalie.wong@wildfireapp.com>
2012-08-04 17:56:19 +0100

    micropost factory with user associations

---
+++ spec/factories/micropost_factory.rb
@@ -0,0 +1,12 @@
+require 'factory_girl'
+
+FactoryGirl.define do
+  factory :micropost do |f|
+    f.association :user
+    f.content "hello"
+  end
+
+  factory :invalid_micropost, parent: :micropost do |f|
+    f.content "ABC"
+  end
+end
