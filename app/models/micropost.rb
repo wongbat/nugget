7abbd42d13bdfe3cb0b83d4d9eb6f512593e8412
Nathalie Wong <nathalie.wong@wildfireapp.com>
2012-08-06 19:29:23 +0100

    added validation for user presence in microposts

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,7 +1,7 @@
 class Micropost < ActiveRecord::Base
   attr_accessible :content, :user_id
-  
+
   belongs_to :user
-  
+  validates :user, :presence => true
   validates :content, :length => { :maximum => 140 }
 end
